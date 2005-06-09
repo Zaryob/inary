1c828e424e785eb3bf1dd33fc291c0fe8280210d
A. Murat Eren <meren@pardus.org.tr>
2005-06-09 11:23:32 +0000

    indentation'ın ağzına iki tokat vurasım var.

--- src/fetcher.py
+++ src/fetcher.py
@@ -12,69 +12,69 @@ from sys import exit
 import pisiconfig
 
 class FetchError (Exception):
-	pass
+    pass
 
 class Fetcher:
-	"""Yet another Pisi tool for fetching files from various sources.."""
-	def __init__(self, uri):
-		self.uri = uri
-		self.filedest = pisiconfig.archives_dir
-		self.scheme = "file"
-		self.netloc = ""
-		self.filepath = ""
-		self.filename = ""
+    """Yet another Pisi tool for fetching files from various sources.."""
+    def __init__(self, uri):
+        self.uri = uri
+        self.filedest = pisiconfig.archives_dir
+        self.scheme = "file"
+        self.netloc = ""
+        self.filepath = ""
+        self.filename = ""
 
-	def fetch (self):
-		from string import split
-		u = urlparse.urlparse(self.uri)
-		self.scheme, self.netloc, self.filepath = u[0], u[1], u[2]
-		self.filename = split(self.filepath, "/")[-1:][0]
-		
-		if self.filename == "":
-			self.err("filename error")
+    def fetch (self):
+        from string import split
+        u = urlparse.urlparse(self.uri)
+        self.scheme, self.netloc, self.filepath = u[0], u[1], u[2]
+        self.filename = split(self.filepath, "/")[-1:][0]
 
-		if os.access(self.filedest, os.W_OK) == False:
-			self.err("no perm to write destination")
+        if self.filename == "":
+            self.err("filename error")
 
-		scheme_err = lambda: self.err("unexpected scheme (expecting file, http or ftp)")
+        if os.access(self.filedest, os.W_OK) == False:
+            self.err("no perm to write destination")
 
-		actions = {
-			'file': self.fetchLocalFile,
-			'http': self.fetchRemoteFile,
-			'ftp' : self.fetchRemoteFile			
-			}; actions.get(self.scheme, scheme_err)()
-	
-	def fetchLocalFile (self):
-		from shutil import copyfile
+        scheme_err = lambda: self.err("unexpected scheme (expecting file, http or ftp)")
 
-		if os.access(self.filepath, os.F_OK) == False:
-			self.err("no such file or no perm to read")
+        actions = {
+            'file': self.fetchLocalFile,
+            'http': self.fetchRemoteFile,
+            'ftp' : self.fetchRemoteFile
+            }; actions.get(self.scheme, scheme_err)()
 
-		copyfile(self.filepath, self.filedest + "/" + self.filename)	
-		
-	def fetchRemoteFile (self):
-		try:
-			file = urllib.urlopen(self.uri)
+    def fetchLocalFile (self):
+        from shutil import copyfile
 
-		except IOError, e:
-			self.err(e.strerror[1])
+        if os.access(self.filepath, os.F_OK) == False:
+            self.err("no such file or no perm to read")
 
-		dest = open(self.filedest + "/" + self.filename , "w")
-		dest.write(file.read())
-	
-	def err (self, error):
-		raise FetchError, error
-	
+        copyfile(self.filepath, self.filedest + "/" + self.filename)
+
+    def fetchRemoteFile (self):
+        try:
+            file = urllib.urlopen(self.uri)
+
+        except IOError, e:
+            self.err(e.strerror[1])
+
+        dest = open(self.filedest + "/" + self.filename , "w")
+        dest.write(file.read())
+
+    def err (self, error):
+        raise FetchError, error
+
 def usage():
-	print "Usage: %s URI" % argv[0]
+    print "Usage: %s URI" % argv[0]
 
 def main():
-	if len(argv) != 2:
-		usage()
-		exit(1)
-		
-	f = Fetcher(argv[1])
-	f.fetch()
+    if len(argv) != 2:
+        usage()
+        exit(1)
+
+    f = Fetcher(argv[1])
+    f.fetch()
 
 if __name__ == "__main__":
-	main()
+    main()
