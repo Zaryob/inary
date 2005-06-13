2c2849525801761ba8e480b1fb88c9c6fa37b246
Eray Özkural <eray@pardus.org.tr>
2005-06-13 10:18:07 +0000

    * remove a guideline

--- src/pisi/installdb.py
+++ src/pisi/installdb.py
@@ -3,7 +3,23 @@
 
 import shelve
 
-d = shelve.open( db_dir + '/fuck yeah')
+d = shelve.open( db_dir + '/install.dbm')
+
+class InstallDBError(Exception):
+    pass
+
+
+
+def isRecorded(name, version, release):
+    return d.has_key( (name, version, release) )
 
 def isInstalled(name, version, release):
+    return d.has_key( (name, version, release) )
+
+def install( name, version, release, state):
+    if isInstalled(name,version,release):
+        raise InstallDBError("already installed")
+    d[ (name,version,release) ] = state
     
+def remove( name, version, release):
+    if 
