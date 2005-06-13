10b8e5f87671e1dd69c088c1466a583ef276c511
Eray Özkural <eray@pardus.org.tr>
2005-06-13 14:14:49 +0000

    * correct typo

--- src/pisi/sourcedb.py
+++ src/pisi/sourcedb.py
@@ -12,7 +12,7 @@ util.check_dir( config.db_dir() )
 d = shelve.open( config.db_dir() + '/source.bdb')
 
 def add_source( name, source_info ):
-    d[name] = package_info
+    d[name] = source_info
 
 def remove_source(name):
     del d[name]
