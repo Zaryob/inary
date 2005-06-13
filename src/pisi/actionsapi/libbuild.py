0315c95db26de241c6d6e8b21e5d11766f17db97
S.Çağlar Onur <caglar@pardus.org.tr>
2005-06-13 11:36:45 +0000

    pisiconfig --> config, genel işleri tutmak için util.py eklendi [ başka isim önerisi? ]

--- src/pisi/actionsapi/libbuild.py
+++ src/pisi/actionsapi/libbuild.py
@@ -6,14 +6,14 @@ from libtoolize import *
 from autotools import *
 
 sys.path.append('..')
-import pisiconfig
+import config
 
 
 if __name__ == "__main__":
 	''' WILLBE: Unpack module will pass the package name to action script '''
 	package_name = 'popt-1.7'
 	''' WILLBE: pisi-build call action script after entered the build directory '''
-	os.chdir( pisiconfig.tmp_dir + '/' + package_name + '/build' )
+	os.chdir( config.tmp_dir + '/' + package_name + '/build' )
 
 	gnuconfig_update()
 	libtoolize()
--- src/pisi/actionsapi/libtoolize.py
+++ src/pisi/actionsapi/libtoolize.py
@@ -4,11 +4,11 @@
 import os,sys
 
 sys.path.append('..')
-import pisiconfig
+import config
 
 def libtoolize():
         ''' FIXME: Düzgün hale getirilecek '''
 	''' patch source with ltmain patches '''
 
-	os.system( 'patch -sN < ' + pisiconfig.lib_dir + '/portage-1.4.1.patch' )
-	os.system( 'patch -sN < ' + pisiconfig.lib_dir + '/sed-1.4.0.patch' )
+	os.system( 'patch -sN < ' + config.lib_dir + '/portage-1.4.1.patch' )
+	os.system( 'patch -sN < ' + config.lib_dir + '/sed-1.4.0.patch' )
