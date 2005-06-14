07a9eec6c24286609c2e624f413530a2d3cee6c3
Barış Metin <baris@pardus.org.tr>
2005-06-14 13:57:23 +0000

    archive'i handle ederken targetDir parametresi ile nereye açılacağına karar veriyoruz (henüz bu özellik çalışmıyor).
    Belki de targetDir'ı constructor'a değil unpac()'e vermeliyiz... Bu
    haline bir bakın yine de...

--- src/pisi/config.py
+++ src/pisi/config.py
@@ -6,6 +6,13 @@ db_dir_suffix = "/var/db/pisi"
 archives_dir_suffix = "/var/cache/pisi/archives"
 tmp_dir_suffix =  "/var/tmp/pisi"
 
+# directory suffixes for build
+# I'm not comfortable with these and accompanying functions,
+# but could find a cleaner way for now.
+build_work_dir_suffix = "/work"
+build_install_dir_suffix  = "/install"
+
+
 destdir = ''                           # install default to root by default
 
 destdir = './tmp'                      # only for ALPHA
@@ -23,3 +30,11 @@ def archives_dir():
 
 def tmp_dir():
     return destdir + tmp_dir_suffix
+
+def build_work_dir(packageName, version, release):
+    packageDir = packageName + '-' + version + '-' + release
+    return destdir + '/' + packageDir + build_work_dir_suffix
+
+def build_install_dir(packageName, version, release):
+    packageDir = packageName + '-' + version + '-' + release
+    return destdir + '/' + packageDir + build_install_dir_suffix
