ef824ebc5b7f86e8845459a6c58bd8bf6663f019
S.Çağlar Onur <caglar@pardus.org.tr>
2005-06-15 12:49:58 +0000

    Check action script for syntax errors...

--- src/pisi/build.py
+++ src/pisi/build.py
@@ -60,7 +60,13 @@ class PisiBuild:
 	# (from the assumption is evil dept.)
 	os.chdir( config.build_work_dir( self.spec.source.name, self.spec.source.version, self.spec.source.release ) + "/" + self.spec.source.name + "-" + self.spec.source.version)
 	locals = globals = {}
-	exec compile( self.actionScript , "error", "exec" ) in locals,globals
+	
+	try:
+		exec compile( self.actionScript , "error", "exec" ) in locals,globals
+	except SyntaxError, e:
+		print "Error : %s" % e
+		return 
+		
 	ui.info("Configuring %s...\n" % self.spec.source.name)
 	locals['src_setup']()
 	ui.info("Building %s...\n" % self.spec.source.name)
