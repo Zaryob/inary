346229e4e71ce99988e6fa06a2ec154b2103f491
Barış Metin <baris@pardus.org.tr>
2005-06-16 10:19:11 +0000

    indent hatası. hiç sevmem!

--- src/pisi/context.py
+++ src/pisi/context.py
@@ -16,11 +16,11 @@ class Constants:
 	    # Binding an attribute once to a const is available
 	    self.__dict__[name] = value
 
-	    def __delattr__(self, name):
-		if self.__dict__.has_key(name):
-		    raise self.ConstError, "Can't unbind constant: %s" % name
-		# we don't have an attribute by this name
-		raise NameError, name
+	def __delattr__(self, name):
+	    if self.__dict__.has_key(name):
+		raise self.ConstError, "Can't unbind constant: %s" % name
+	    # we don't have an attribute by this name
+	    raise NameError, name
 
     c = __const()
 
