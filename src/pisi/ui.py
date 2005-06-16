9711df9de54730b4d4027cb0c8c8c435d4c98218
Barış Metin <baris@pardus.org.tr>
2005-06-16 17:57:49 +0000

    Renklendirme için kendi yöntemimizi de yapabilirz biz, değil mi? ;)

--- src/pisi/ui.py
+++ src/pisi/ui.py
@@ -1,7 +1,7 @@
 # generic user interface
 
 import sys
-from output import *
+from colors import colorize
 
 # put the interface directly in the module
 # since the UI is _unique_
@@ -19,11 +19,11 @@ def error(msg):
 # default UI implementation
 class CLI:
     def info(self, msg):
-        sys.stdout.write(blue(msg))
+        sys.stdout.write(colorize(msg, 'blue'))
         sys.stdout.flush()
 
     def error(self,msg):
-        sys.stdout.write(bold(red(msg)))
+        sys.stdout.write(colorize(msg, 'red'))
         sys.stdout.flush()
 # default UI is CLI
 impl = CLI()
