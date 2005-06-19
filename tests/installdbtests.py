2919c070851dc1e69fb22ab599038b77581ed2f5
Eray Özkural <eray@pardus.org.tr>
2005-06-19 23:23:25 +0000

    * testleri de duzelt(meye calis...)

--- tests/installdbtests.py
+++ tests/installdbtests.py
@@ -12,24 +12,24 @@ class InstallDBTestCase(unittest.TestCase):
         pass
 
     def testRemoveDummy(self):
-        installdb.remove('installtest', '0.1', '2')
-        self.assert_(not installdb.is_installed('installtest', '0.1', '2'))
+        installdb.remove('installtest')
+        self.assert_(not installdb.is_installed('installtest'))
         
     def testInstall(self):
-        installdb.remove('installtest', '0.1', '2')
+        installdb.remove('installtest')
         installdb.install('installtest', '0.1', '2', './tests/sandbox/files.xml')
-        f = installdb.files('installtest', '0.1', '2')
+        f = installdb.files('installtest')
         a = f.readlines()
         self.assertEqual(a[0], 'placeholder\n')
-        self.assert_(installdb.is_installed('installtest', '0.1', '2'))
+        self.assert_(installdb.is_installed('installtest')
 
     def testRemovePurge(self):
         installdb.install('installtest', '0.1', '2', './tests/sandbox/files.xml')
-        self.assert_(installdb.is_installed('installtest', '0.1', '2'))
-        installdb.remove('installtest', '0.1', '2')
-        self.assert_(installdb.is_removed('installtest', '0.1', '2'))
+        self.assert_(installdb.is_installed('installtest'))
+        installdb.remove('installtest')
+        self.assert_(installdb.is_removed('installtest'))
         installdb.purge('installtest', '0.1', '2')
-        self.assert_(not installdb.is_recorded('installtest', '0.1', '2'))
-        self.assert_(not os.access(installdb.files_name('installtest', '0.1', '2'), os.F_OK))
+        self.assert_(not installdb.is_recorded('installtest'))
+        self.assert_(not os.access(installdb.files_name('installtest'), os.F_OK))
 
 suite = unittest.makeSuite(InstallDBTestCase)
