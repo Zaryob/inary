4a27d39b98c164b180fa241a648a19a5dc269cfb
Eray Özkural <eray@pardus.org.tr>
2005-06-20 14:06:49 +0000

    * start implementing progress stuff in CLI

--- pisi/ui.py
+++ pisi/ui.py
@@ -12,6 +12,13 @@ class CLI:
     def __init__(self, debuggy = True):
         self.showDebug = debuggy
 
+    class Progress:
+        def __init__(self, name, symbol):
+            self.name = name
+            self.percent = 0
+            self.rate = 0
+            self.symbol = symbol
+
     def info(self, msg):
         sys.stdout.write(colorize(msg, 'blue'))
         sys.stdout.flush()
@@ -24,6 +31,20 @@ class CLI:
     def error(self,msg):
         sys.stdout.write(colorize(msg, 'red'))
         sys.stdout.flush()
+
+    def startProgress(self, name, symbol):
+        ## support one progress for now
+        self.activeProgress = Progress(name, symbol)
+
+    def updateProgress(self, percent, rate):
+        self.activeProgress.percent = percent
+        self.activeProgress.rate = rate
+        self.displayProgress(pd)
+
+    def displayProgress(pd):
+        out = '\r%-30.30s %3d%% %12.2f %s' % \
+              (self.name, self.percent, self.rate, self.symbol)
+        self.info(out)
 
 # default UI is CLI
 ui = CLI()
