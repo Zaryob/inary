c9b76ce3cb0d1f7680bf0b5bf52223e1a0ac933c
S.Çağlar Onur <caglar@pardus.org.tr>
2005-06-21 13:14:36 +0000

    Olmayan test suite çıkartıldı [ yerine konunca ekleriz ], birazdan autotools gelecek, böcek ezmekle uğraşıyorum actionsapi içinde

--- pisi/actionsapi/gnuconfig.py
+++ pisi/actionsapi/gnuconfig.py
@@ -4,6 +4,8 @@
 import os, string, re, shutil
 from shell import *
 
+from pisi.ui import ui
+
 def gnuconfig_findnewest():
     ''' find the newest config.* file according to
     timestamp and return it'''
@@ -15,25 +17,26 @@ def gnuconfig_findnewest():
 		 '/usr/share/automake-1.6/config.sub',
 		 '/usr/share/automake-1.5/config.sub',
 		 '/usr/share/automake-1.4/config.sub',
-                 'share/config.sub']
+         'share/config.sub']
     
     newer_location = {}
 
     for i in locations:
         if not os.path.exists(i):
             continue
-        newer_location[i] = re.sub('\'',
-				   '',
-				   string.split((cat(i) | 
-						 tr(str.rstrip) | 
-						 grep ('^timestamp') |
-						 join), '=')[1])
+        newer_location[i] = re.sub('\'', '',
+                                   string.split((cat(i) | tr(str.rstrip)
+                                   | grep ('^timestamp') 
+                                   | join), '=')[1])
 
     keys = newer_location.keys()
     keys.sort()
-    map(newer_location.get, keys)
 
-    return os.path.dirname(newer_location.popitem()[0])
+    thelist=[]
+    for i in keys:
+        thelist.append((i, newer_location[i]))
+
+    return os.path.dirname(thelist[0][0])
 
 def gnuconfig_update():
     ''' copy newest config.* onto source's '''
@@ -42,11 +45,11 @@ def gnuconfig_update():
 
     try:
         shutil.copyfile(newer_location + '/config.sub',
-			os.getcwd() + '/config.sub')
+            os.getcwd() + '/config.sub')
         shutil.copyfile(newer_location + '/config.guess',
-			os.getcwd() + '/config.guess')
-    except IOError:
-        print 'Hata mata...'
+            os.getcwd() + '/config.guess')
+    except IOError, e:
+        ui.error ("Error : %s\n" % e)
         sys.exit()
 
-    print 'GNU Config Update Finished...'
+    ui.info('GNU Config Update Finished...\n')
