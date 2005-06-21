fd22e78794ac52394fc1ae32fa9e5a06f7176691
S.Çağlar Onur <caglar@pardus.org.tr>
2005-06-21 14:17:33 +0000

    /usr/lib/python2.3/tabnanny.py context.py

--- pisi/context.py
+++ pisi/context.py
@@ -10,43 +10,43 @@ class Constants:
     c = oo.const()
 
     def __init__(self):
-	self.c.lib_dir_suffix = "/var/lib/pisi"
-	self.c.db_dir_suffix = "/var/db/pisi"
-	self.c.archives_dir_suffix = "/var/cache/pisi/archives"
-	self.c.tmp_dir_suffix =  "/var/tmp/pisi"
+        self.c.lib_dir_suffix = "/var/lib/pisi"
+        self.c.db_dir_suffix = "/var/db/pisi"
+        self.c.archives_dir_suffix = "/var/cache/pisi/archives"
+        self.c.tmp_dir_suffix =  "/var/tmp/pisi"
 
-	# directory suffixes for build
-	self.c.work_dir_suffix = "/work"
-	self.c.install_dir_suffix  = "/install"
+        # directory suffixes for build
+        self.c.work_dir_suffix = "/work"
+        self.c.install_dir_suffix  = "/install"
 
-	# file/directory names
-	self.c.actions_file = "actions.py"
-	self.c.files_dir = "files"
+        # file/directory names
+        self.c.actions_file = "actions.py"
+        self.c.files_dir = "files"
 
-	# functions in actions_file
-	self.c.setup_func = "setup"
-	self.c.build_func = "build"
-	self.c.install_func = "install"
+        # functions in actions_file
+        self.c.setup_func = "setup"
+        self.c.build_func = "build"
+        self.c.install_func = "install"
 
     def __getattr__(self, attr):
- 	return getattr(self.c, attr)
+        return getattr(self.c, attr)
 
     def __setattr__(self, attr, value):
- 	return setattr(self.c, attr, value)
+        return setattr(self.c, attr, value)
 
     def __delattr__(self, attr):
-	return delattr(self.c, attr)
+        return delattr(self.c, attr)
 
 class Context(object):
     """Config/Context Singleton"""
     class __impl:
-	def __init__(self):
+    def __init__(self):
 	    self.const = Constants()
 	    # self.c.destdir = ''       # install default to root by default
 	    self.destdir = './tmp'    # only for ALPHA
 	    # the idea is that destdir can be set with --destdir=...
 
-        def setSpecFile(self, pspecfile):
+    def setSpecFile(self, pspecfile):
 	    self.pspecfile = pspecfile
 	    spec = SpecFile()
 	    spec.read(pspecfile)
@@ -58,29 +58,29 @@ class Context(object):
         # x_dir: system wide directory for storing info type x
         # pkg_x_dir: per package directory for storing info type x
 
-	def lib_dir(self):
+    def lib_dir(self):
 	    return self.destdir + self.const.lib_dir_suffix
 
-	def db_dir(self):
+    def db_dir(self):
 	    return self.destdir + self.const.db_dir_suffix
 
-	def archives_dir(self):
+    def archives_dir(self):
 	    return self.destdir + self.const.archives_dir_suffix
 	
-	def tmp_dir(self):
+    def tmp_dir(self):
 	    return self.destdir + self.const.tmp_dir_suffix
 
-	def pkg_dir(self):
+    def pkg_dir(self):
 	    packageDir = self.spec.source.name + '-' \
 		+ self.spec.source.version + '-' + self.spec.source.release
 
 	    return self.destdir + self.const.tmp_dir_suffix \
 		+ '/' + packageDir
 	
-	def pkg_work_dir(self):
+    def pkg_work_dir(self):
 	    return self.pkg_dir() + self.const.work_dir_suffix
 
-	def pkg_install_dir(self):
+    def pkg_install_dir(self):
 	    return self.pkg_dir() + self.const.install_dir_suffix
 
     __instance = __impl()
@@ -90,10 +90,10 @@ class Context(object):
             self.__instance.setSpecFile(pspecfile)
 
     def __getattr__(self, attr):
- 	return getattr(self.__instance, attr)
+        return getattr(self.__instance, attr)
 
     def __setattr__(self, attr, value):
- 	return setattr(self.__instance, attr, value)
+        return setattr(self.__instance, attr, value)
 
 
 # create a default context WITH NO PSPEC
