4d82f74e7ee6d149aa0d801996e067a046792a4b
Barış Metin <baris@pardus.org.tr>
2005-06-28 09:22:14 +0000

    fetcher testini düzelt.

--- tests/fetchertests.py
+++ tests/fetchertests.py
@@ -5,17 +5,21 @@ import os
 from pisi import fetcher
 from pisi import util
 from pisi import context
+from pisi import config
+from pisi import purl
 
 class FetcherTestCase(unittest.TestCase):
     def setUp(self):
         self.ctx = context.BuildContext("samples/popt/popt.pspec")
-        self.fetch = fetcher.Fetcher(self.ctx.spec.source)
+        self.url = purl.PUrl(self.ctx.spec.source.archiveUri)
+        self.destpath = config.config.archives_dir()
+        self.fetch = fetcher.Fetcher(self.url, self.destpath)
     
     def testFetch(self):
         self.fetch.fetch()
-        destpath = self.fetch.filedest + "/" + self.fetch.filename
-        if os.access(destpath, os.R_OK):
-            self.assertEqual(util.sha1_file(destpath),
+        fetchedFile = os.path.join(self.destpath, self.url.filename())
+        if os.access(fetchedFile, os.R_OK):
+            self.assertEqual(util.sha1_file(fetchedFile),
                  self.ctx.spec.source.archiveSHA1)
 
 suite = unittest.makeSuite(FetcherTestCase)
