251311ade712347d795be34807af0650a3da6498
Barış Metin <baris@pardus.org.tr>
2005-07-04 15:24:06 +0000

    bash için actions.py'yi biraz düzenledim. bash...ebuild'de install sırasında bir takım daha işler yapılıyor(install dir içerisinde cp/symlink vs.) onları da halletmek gerekiyor.
    Çağlar. Bunun için actionsAPI'nin install_dir içerisinde de
    çalışabilir olması lazım. glb.evn ile bu mümkün...

--- samples/bash/actions.py
+++ samples/bash/actions.py
@@ -3,12 +3,18 @@
 
 from pisi.actionsapi import gnuconfig
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 
 
 def setup():
     gnuconfig.gnuconfig_update()
-#    libtoolize.libtoolize()
-    autotools.configure( '--with-nls' )
+
+    pisitools.dosed("s:-lcurses:-lncurses:", "configure")
+
+    autotools.configure("--with-nls --disable-profiling --without-gnu-malloc")
+
+    pisitools.dosed("/^TERMCAP_LIB/s:-lncurses:-Wl,-Bstatic -lncurses -Wl,-Bdynamic:",
+                    "Makefile")
 
 def build():
     autotools.make()
