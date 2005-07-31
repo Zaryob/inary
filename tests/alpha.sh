e3f675893032e603fb703ae446b8fdb6fb89a4ba
Eray Özkural <eray@pardus.org.tr>
2005-07-31 15:45:18 +0000

    * buna da hafiften basla

--- tests/alpha.sh
+++ tests/alpha.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-pwd
+echo "alpha functionality test script"
+echo "working directory:" `pwd`
 #rm -rf tmp
-pisi-cli build https://svn.uludag.org.tr/pisi/trunk/z/zip/pspec.xml https://svn.uludag.org.tr/pisi/trunk/u/unzip/pspec.xml
-pisi-cli -index .
-pisi-cli -updatedb pisi-index.xml
-pisi-cli -install start*pisi
-pisi-cli -install popt*pisi
-find tmp -iname '*.bdb' | xargs ./cat-db.py
+echo "*** build tests"
+pisi-cli build https://svn.uludag.org.tr/pisi/trunk/z/zip/pspec.xml \
+ https://svn.uludag.org.tr/pisi/trunk/u/unzip/pspec.xml
+echo "*** repository tests"
+
+echo "*** package ops"
+
+for x in tmp -iname '*.bdb'; do
+    echo "contents of database " $x;
+    cat-db.py;
+done
