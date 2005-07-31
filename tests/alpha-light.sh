80f34205e3156be187e6cdf481d4c50aa7c65c11
Eray Özkural <eray@pardus.org.tr>
2005-07-31 15:41:45 +0000

    * pisi root'dan calisiyor bunlar

--- tests/alpha-light.sh
+++ tests/alpha-light.sh
@@ -3,12 +3,10 @@
 pwd
 PATH=$PATH:.
 set -x
-pisi-cli build https://svn.uludag.org.tr/pisi/trunk/z/zip/pspec.xml https://svn.uludag.org.tr/pisi/trunk/u/unzip/pspec.xml
+pisi-cli build tests/zip/pspec.xml tests/unzip/pspec.xml
 pisi-cli index .
 pisi-cli add-repo repo1 pisi-index.xml
 pisi-cli update-repo repo1
-# database contents
-#find tmp -iname '*.bdb' | xargs tools/cat-db.py
 pisi-cli install zip
 pisi-cli list-installed
 pisi-cli remove unzip
