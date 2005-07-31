a44854ec7afbdb6e67d836ab9d29e450db30b2a9
Barış Metin <baris@pardus.org.tr>
2005-07-31 21:41:51 +0000

    packagedbtests çalışsın.

--- tests/packagedbtests.py
+++ tests/packagedbtests.py
@@ -9,16 +9,20 @@ from pisi import context
 class PackageDBTestCase(unittest.TestCase):
 
     def setUp(self):
+        # setUp will be called for each test individually
         self.ctx = context.BuildContext('tests/popt/pspec.xml')
         self.pdb = PackageDB('testdb')
         
     def testAdd(self):
         self.pdb.add_package(self.ctx.spec.packages[0])
         self.assert_(self.pdb.has_package('popt-libs'))
+        # close the database and remove lock
+        del self.pdb
     
     def testRemove(self):
         self.pdb.remove_package('popt-libs')
         self.assert_(not self.pdb.has_package('popt-libs'))
+        del self.pdb
 
 suite = unittest.makeSuite(PackageDBTestCase)
 
