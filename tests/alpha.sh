ef56f6948af6e5fa045177c96229f346516464eb
Eray Özkural <eray@pardus.org.tr>
2005-07-31 15:39:41 +0000

    * basitlestirelim olayi -light icin, tamyagli farkli ama

--- tests/alpha.sh
+++ tests/alpha.sh
@@ -2,8 +2,7 @@
 
 pwd
 #rm -rf tmp
-pisi-cli build packages/*/*/pspec.xml
-
+pisi-cli build https://svn.uludag.org.tr/pisi/trunk/z/zip/pspec.xml https://svn.uludag.org.tr/pisi/trunk/u/unzip/pspec.xml
 pisi-cli -index .
 pisi-cli -updatedb pisi-index.xml
 pisi-cli -install start*pisi
