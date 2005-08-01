f2555a80238322432ae022dda2f2c425464d0549
Barış Metin <baris@pardus.org.tr>
2005-08-01 00:15:10 +0000

    setup.py dosyamızı düzenledim.
    Ek olarak dışarıdan test edenlerin kullanmaları için bir version'a svn
    revision'ı ekleyen bir kod koydum.
    
    Buradan setup.py sdist ile tar arşivi oluşturup dağıttığımızda insanlar
    hangi svn sürümünden oluşturulmuş pisi'yi kullandığını da görmüş
    olurlar. Tabi bu özelliği ileride kaldırırız ya da
    değiştiririz. Şimdilik güzel olur diye düşündüm :).
    
    Daha önce distutils kullanmamış olanlar için:
    
    $ sudo python setup.py install
    
    pisi'yi sisteme kurar
    
    
    $ python setup.py sdist
    
    bir tar arşivi oluşturup 'dist' dizinine yerleştirir.

--- setup.py
+++ setup.py
@@ -15,16 +15,38 @@
 
 from distutils.core import setup
 
+PISI_VERSION = "0.1"
+
+def getRevision():
+    import os
+    try:
+        p = os.popen("svn info 2> /dev/null")
+        for line in p.readlines():
+            line = line.strip()
+            if line.startswith("Revision:"):
+                return line.split(":")[1].strip()
+    except:
+        pass
+
+    # doesn't working in a Subversion directory
+    return None
+
+def getVersion():
+    rev = getRevision()
+    if rev:
+        return "-r".join([PISI_VERSION, rev])
+    else:
+        return PISI_VERSION
+
 setup(name="pisi",
-      version="0.1",
-      description="Pardus Package Manager",
-      long_description="blah blah blah",
-      license="GNU GPL",
+      version= getVersion(),
+      description="PISI (Packages Installed Successfully as Intended)",
+      long_description="PISI is the package management system of Pardus Linux.",
+      license="GNU GPL2",
       author="Pardus Developers",
-      author_email="hotmail@uludag.org.tr",
-      url="http://uludag.org.tr",
+      author_email="pisi@uludag.org.tr",
+      url="http://www.uludag.org.tr/eng/pisi/",
       package_dir = {'': ''},
       packages = ['pisi', 'pisi.cli', 'pisi.actionsapi'],
-      package_data = {'pisi.actionsapi' : ['share/*'] }
-      scripts = ['pisi-build', 'pisi-install']
+      scripts = ['pisi-cli']
      )
