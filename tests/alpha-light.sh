bb4c6dc1eeb2f92f2c0af0da3897cf23e34d0065
Eray Özkural <eray@pardus.org.tr>
2005-08-08 20:05:42 +0000

    * make the config.options bug clear

--- tests/alpha-light.sh
+++ tests/alpha-light.sh
@@ -7,7 +7,7 @@ pisi-cli build tests/zip/pspec.xml tests/unzip/pspec.xml
 pisi-cli index .
 pisi-cli add-repo repo1 pisi-index.xml
 pisi-cli update-repo repo1
-pisi-cli install zip
+pisi-cli --ignore-comar install zip
 pisi-cli list-installed
-pisi-cli remove unzip
-pisi-cli install zip*.pisi
+pisi-cli --ignore-comar remove unzip
+pisi-cli --ignore-comar install zip*.pisi
