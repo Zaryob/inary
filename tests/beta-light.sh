e920d22e7f87f0b46f31d078d081fa7e7fb0c046
Eray Özkural <eray@pardus.org.tr>
2005-10-19 14:09:35 +0000

    minor

--- tests/beta-light.sh
+++ tests/beta-light.sh
@@ -3,10 +3,12 @@
 pwd
 PATH=$PATH:.
 set -x
+pisi-cli -Dtmp clean
 pisi-cli -B -Dtmp remove unzip
 set -e
 pisi-cli -Dtmp -E --ignore-build-no build tests/zip/pspec.xml tests/unzip/pspec.xml
 pisi-cli -Dtmp index .
+#pisi-cli -Dtmp remove-repo repo1
 pisi-cli -Dtmp add-repo repo1 pisi-index.xml
 pisi-cli -Dtmp list-repo
 pisi-cli -Dtmp update-repo repo1
