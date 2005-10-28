68bda67bed13d9c7ba00da65aef93a8d31e733fa
S.Çağlar Onur <caglar@pardus.org.tr>
2005-10-28 19:37:07 +0000

    update-environ çalışsın...

--- tools/update-environ.py
+++ tools/update-environ.py
@@ -3,10 +3,12 @@
 
 # Generates /etc/ld.so.conf and /etc/profile.env according to /etc/env.d directory
 
-import sys
-
-sys.path.append('.')
-import pisi.util as util
+import pisi
+import pisi.api
+import pisi.util
+import pisi.config
 
 if __name__ == "__main__":
-    util.env_update()
+    options = pisi.config.Options()
+    pisi.api.init(options = options, comar = False, database = False)
+    pisi.util.env_update()
