2d9642a787dcad7785cc3bde462b599d3519ecb5
Eray Özkural <eray@pardus.org.tr>
2005-11-17 20:10:57 +0000

    yanlış bilgi girilmesin

--- tools/ebuild2pisi.py
+++ tools/ebuild2pisi.py
@@ -30,7 +30,6 @@ PSPEC_TEMPLATE='''<?xml version="1.0" encoding="utf-8" standalone="no"?>
             <Email>PACKAGER_EMAIL</Email>
         </Packager>
         <License>%(license)s</License>
-        <IsA>category</IsA>
         <Summary xml:lang="en">%(description)s</Summary>
         <Description xml:lang="en">%(description)s</Description>
         <Archive type="%(archiveType)s" sha1sum="SUM">%(archiveUri)s</Archive>
