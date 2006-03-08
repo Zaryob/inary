ce6000330db1d6ed0069da8a60e654c8b272767e
Eray Özkural <eray@pardus.org.tr>
2006-03-08 11:40:05 +0000

    * add pardus-1.0 by default

--- pisi/cli/commands.py
+++ pisi/cli/commands.py
@@ -787,8 +787,8 @@ NB: We support only local files (e.g., /a/b/c) and http:// URIs at the moment
                 name = self.args[0]
                 indexuri = self.args[1]
             else:
-                name = 'pardus-devel'
-                indexuri = 'http://paketler.uludag.org.tr/pardus-devel/pisi-index.xml'
+                name = 'pardus-1.0'
+                indexuri = 'http://paketler.uludag.org.tr/pardus-1.0/pisi-index.xml'
             pisi.api.add_repo(name, indexuri)
             if ctx.ui.confirm(_('Update PISI database for repository %s?') % name):
                 pisi.api.update_repo(name)
