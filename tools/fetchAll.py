a9d360b04e4eea800d2426185939a73c44abfd5e
S.Çağlar Onur <caglar@pardus.org.tr>
2006-03-15 10:25:33 +0000

    ben bu apiyi sevmiyorum......

--- tools/fetchAll.py
+++ tools/fetchAll.py
@@ -14,7 +14,9 @@ import sys
 
 sys.path.append('.')
 
+import pisi.api
 import pisi.uri
+import pisi.context as ctx
 import pisi.specfile
 import pisi.util as util
 from pisi.fetcher import fetch_url
@@ -30,9 +32,13 @@ def scanPSPEC(folder):
     return packages
 
 def isCached(file, sha1sum):
-    return util.check_file_hash(os.path.join(config.archives_dir(), file), sha1sum)
+    try:
+        return util.check_file_hash(os.path.join(ctx.config.archives_dir(), file), sha1sum)
+    except:
+        pass
 
 if __name__ == "__main__":
+    pisi.api.init(database=False, options='')
     try:
         packages = scanPSPEC(sys.argv[1])
     except:
@@ -43,14 +49,15 @@ if __name__ == "__main__":
         spec = pisi.specfile.SpecFile()
         spec.read(os.path.join(package, "pspec.xml"))
 
-        URI = pisi.uri.URI(spec.source.archiveUri)
+        URI = pisi.uri.URI(spec.source.archive.uri)
 
-        if not isCached(URI.filename(), spec.source.archiveSHA1):
-            print URI, " -> " , os.path.join(config.archives_dir(), URI.filename())
+        if not isCached(URI.filename(), spec.source.archive.sha1sum):
+            print URI, " -> " , os.path.join(ctx.config.archives_dir(), URI.filename())
             try:
-                fetch_url(URI, config.archives_dir())
+                fetch_url(URI, ctx.config.archives_dir())
             except pisi.fetcher.FetchError, e:
                 print e
                 pass
         else:
             print URI, "already downloaded..."
+    pisi.api.finalize()
