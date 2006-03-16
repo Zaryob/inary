bc60d436dd31742ec51364a70ba0cfb27276f082
Eray Özkural <eray@pardus.org.tr>
2006-03-16 14:08:31 +0000

    * add another test case (now we cover 2294)

--- tests/dependencytests.py
+++ tests/dependencytests.py
@@ -18,18 +18,30 @@ class DependencyTestCase(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test603(self):
+    def testVersionFrom(self):
         # a real life story bug #603
         gtkmmdep = Dependency()
         gtkmmdep.versionFrom = '1.9.1'
         gtkmmdep.package = 'atk'
         self.assert_( not gtkmmdep.satisfies('atk', '1.8.0', '1') )
 
-    def test2294(self):
-        # releaseFrom isn't taken into account
+    def testReleaseFrom(self):
+        # releaseFrom isn't taken into account #2294
         dep = Dependency()
         dep.releaseFrom = '121'
         dep.package = 'dbus'
         self.assert_(not dep.satisfies('dbus', '1.2', '2'))
-    
+
+    def testReleaseTo(self):
+        dep = Dependency()
+        dep.releaseTo = '22'
+        dep.package = 'dbus'
+        self.assert_(not dep.satisfies('dbus', '1.2', '25'))
+
+    def testReleaseIs(self):
+        dep = Dependency()
+        dep.release = '42'
+        dep.package = 'dbus'
+        self.assert_(dep.satisfies('dbus', '1.2', '42'))
+
 suite = unittest.makeSuite(DependencyTestCase)
