6a7811f7c54cbd3d36a72efadb66c0887e6f2c87
Faik Uygur <faik@pardus.org.tr>
2006-04-13 13:00:48 +0000

    imports

--- pisi/zipfileext.py
+++ pisi/zipfileext.py
@@ -1,6 +1,40 @@
-# extends zipfile module with 7zip support
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2006, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+# Authors:  Eray Ozkural <eray@pardus.org.tr>
+#           Faik Uygur   <faik@pardus.org.tr>
+#
+# Extends zipfile module with 7zip and bzip2 support
+
+# python standard library modules
+import os
+import struct
+import time
 
 from zipfile import *
+
+try:
+    import zlib
+except ImportError:
+    zlib = None
+
+try:
+    import bzip2
+except ImportError:
+    bzip2 = None
+
+try:
+    import pylzma
+except ImportError:
+    pylzma = None
 
 ZIP_BZIP2 = 12
 ZIP_7ZIP = 255
