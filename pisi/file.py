ff1fc4414dad35b75cea36682d143f7284932c26
Faik Uygur <faik@pardus.org.tr>
2006-04-20 08:41:37 +0000

    watch it

--- pisi/file.py
+++ pisi/file.py
@@ -110,7 +110,7 @@ class File:
         else:
             localfile = uri.get_uri() #TODO: use a special function here?
             if not os.path.exists(localfile):
-                raise IOError(_("File '\%s'\ not found." % localfile))
+                raise IOError(_("File '\%s'\ not found.") % localfile)
 
         if sha1sum:        
         
