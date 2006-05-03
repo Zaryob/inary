b72eac4b6b0990f6a14f5e188539a4a98d216c2f
S.Çağlar Onur <caglar@pardus.org.tr>
2006-05-03 16:00:17 +0000

    buildfarm'ı kırıyor (exception attığı için), düzeltilene kadar log = False

--- pisi/api.py
+++ pisi/api.py
@@ -69,7 +69,9 @@ def init(database = True, write = True,
 
     
     if os.access('/var/log', os.W_OK):
-        ctx.log = True
+        # FIXME: Breaks buildfarm 
+        #ctx.log = True
+        ctx.log = False
         logging.basicConfig(level=logging.DEBUG,
             format='%(asctime)s %(levelname)-8s %(message)s',
             datefmt='%a, %d %b %Y %H:%M:%S',
