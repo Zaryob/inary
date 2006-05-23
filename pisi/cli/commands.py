b6b5610b21c66928e770147f29d07ecc7ee40c5b
Faik Uygur <faik@pardus.org.tr>
2006-05-23 18:21:40 +0000

    info thingy.

--- pisi/cli/commands.py
+++ pisi/cli/commands.py
@@ -625,7 +625,7 @@ Usage: info <package1> <package2> ... <packagen>
     def __init__(self):
         super(Info, self).__init__()
 
-    name = ("info")
+    name = ("info", None)
 
     def options(self):
         self.parser.add_option("-f", "--files", action="store_true",
