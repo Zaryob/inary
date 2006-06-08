b979d52d36a5aa4d48df9508cb7421878872ea77
Eray Özkural <eray@pardus.org.tr>
2006-06-08 12:32:44 +0000

    * pisicli: we have a uniform exception naming (only name change there) * improve pisish a little

--- pisi/cli/pisicli.py
+++ pisi/cli/pisicli.py
@@ -29,10 +29,6 @@ class ParserError(pisi.Exception):
 class Error(pisi.Error):
     pass
 
-class CLIError(Error):
-    pass
-
-
 class PreParser(OptionParser):
     """consumes any options, and finds arguments from command line"""
 
@@ -98,14 +94,14 @@ class PisiCLI(object):
                     sys.exit(0)
                 elif 'help' in opts or 'h' in opts:
                     self.die()
-                raise CLIError(_('No command given'))
+                raise Error(_('No command given'))
             cmd_name = args[0]
         except ParserError:
-            raise CLIError(_('Command line parsing error'))
+            raise Error(_('Command line parsing error'))
 
         self.command = Command.get_command(cmd_name, args=orig_args)
         if not self.command:
-            raise CLIError(_("Unrecognized command: %s") % cmd_name)
+            raise Error(_("Unrecognized command: %s") % cmd_name)
 
     def die(self):
         printu('\n' + self.parser.format_help())
