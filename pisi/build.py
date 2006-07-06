f8e394c908c1b6da3ab1a86221fed3bfead7d29b
S.Çağlar Onur <caglar@pardus.org.tr>
2006-07-06 12:19:04 +0000

    general -> build

--- pisi/build.py
+++ pisi/build.py
@@ -223,12 +223,12 @@ class Builder:
 
         # First check icecream, if not found use ccache, no need to use both
         # together (according to kde-wiki it cause performance loss)
-        if ctx.config.values.general.buildhelper == "icecream":
+        if ctx.config.values.build.buildhelper == "icecream":
             if os.path.exists("/opt/icecream/bin/gcc"):
                 # Add icecream directory for support distributed compiling :)
                 os.environ["PATH"] = "/opt/icecream/bin/:%s" % os.environ["PATH"]
                 ctx.ui.info(_("IceCream detected. Make sure your daemon is up and running..."))
-        elif ctx.config.values.general.buildhelper == "ccache":
+        elif ctx.config.values.build.buildhelper == "ccache":
             if os.path.exists("/usr/lib/ccache/bin/gcc"):
                 # Add ccache directory for support Compiler Cache :)
                 os.environ["PATH"] = "/usr/lib/ccache/bin/:%s" % os.environ["PATH"]
