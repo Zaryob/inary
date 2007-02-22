c6a229a9c0094022b56a87b0104ab77d7c66d4a9
A. Murat Eren <meren@pardus.org.tr>
2007-02-22 19:28:24 +0000

    update copyright information (spam)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2005-2006, TUBITAK/UEKAE
+# Copyright (C) 2005 - 2007, TUBITAK/UEKAE
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free
--- tests/ootests.py
+++ tests/ootests.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2005, TUBITAK/UEKAE
+# Copyright (C) 2005 - 2007, TUBITAK/UEKAE
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free
