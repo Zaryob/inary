993c75e3b8f1098f67dbaa619549cd5faa889f84
Uğur Çetin <ugur.jnmbk@gmail.com>
2007-06-26 09:11:58 +0000

    update for new specfile package things were removed in r9744

--- tests/specfiletests.py
+++ tests/specfiletests.py
@@ -19,13 +19,13 @@ class SpecFileNewTestCase(unittest.TestCase):
     def setUp(self):
         self.spec = specfile.SpecFile()
         self.spec.read("tests/popt/pspec.xml")
-    
+
     def testFields(self):
         self.assertEqual(self.spec.source.name, "popt")
 
-        self.assertEqual(self.spec.source.version, "1.7")
+        self.assertEqual(self.spec.getSourceVersion(), "1.7")
 
-        self.assertEqual(self.spec.source.release, "1")
+        self.assertEqual(self.spec.getSourceRelease(), "1")
 
         self.assertEqual(self.spec.source.archive.sha1sum,
                          "66f3c77b87a160951b180447f4a6dce68ad2f71b")
@@ -49,24 +49,15 @@ class SpecFileNewTestCase(unittest.TestCase):
 
     def testIsAPartOf(self):
         # test existence in Source
+
         if not "app:console" in self.spec.source.isA:
             self.fail("Failed to match IsA in Source")
         if not isinstance(self.spec.source.isA, list):
             self.fail("source.isA is not a list, but it must be...")
-        
+
         if "system.base" != self.spec.source.partOf:
             self.fail("Failed to match PartOf in Source")
 
-        # test existence in Package
-        pkg = self.spec.packages[0]
-        if not "app:console" in pkg.isA:
-            self.fail("Failed to match IsA in Package")
-        if not isinstance(pkg.isA, list):
-            self.fail("source.isA is not a list, but it must be...")
-
-        if "system.base" != pkg.partOf:
-            self.fail("Failed to match PartOf in Package")
-        
     def testVerify(self):
         if self.spec.errors():
             self.fail("Failed to verify specfile")
