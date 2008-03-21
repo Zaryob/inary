6d570aa53d91a67c9df11c43293c310bbe4c484a
Faik Uygur <faik@pardus.org.tr>
2008-03-21 13:21:45 +0000

    BUG:FIXED:6795

--- pisi/fetcher.py
+++ pisi/fetcher.py
@@ -172,13 +172,13 @@ class Fetcher:
         proxies = {}
 
         if ctx.config.values.general.http_proxy and self.url.scheme() == "http":
-            proxies[pisi.uri.URI(http_proxy).scheme()] = ctx.config.values.general.http_proxy
+            proxies[pisi.uri.URI(ctx.config.values.general.http_proxy).scheme()] = ctx.config.values.general.http_proxy
 
         if ctx.config.values.general.https_proxy and self.url.scheme() == "https":
-            proxies[pisi.uri.URI(https_proxy).scheme()] = ctx.config.values.general.https_proxy
+            proxies[pisi.uri.URI(ctx.config.values.general.https_proxy).scheme()] = ctx.config.values.general.https_proxy
 
         if ctx.config.values.general.ftp_proxy and self.url.scheme() == "ftp":
-            proxies[pisi.uri.URI(ftp_proxy).scheme()] = ctx.config.values.general.ftp_proxy
+            proxies[pisi.uri.URI(ctx.config.values.general.ftp_proxy).scheme()] = ctx.config.values.general.ftp_proxy
 
         if self.url.scheme() in proxies:
             ctx.ui.info(_("Proxy configuration has been found for '%s' protocol") % self.url.scheme())
