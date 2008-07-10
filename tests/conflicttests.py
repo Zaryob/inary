a235e5c65862565d10f206a4088c418ff65ca403
Beyza Ermiş <beyza@pardus.org.tr>
2008-07-10 11:12:56 +0000

    Conflict test case has been added.

---
+++ tests/conflicttests.py
@@ -0,0 +1,28 @@
+import unittest
+import pisi
+import pisi.relation
+import pisi.conflict
+
+class ConflictTestCase(unittest.TestCase):
+    def testInstalledPackageConflicts(self):
+        pisi.api.install(["ethtool"])
+        relation = pisi.relation.Relation()
+        relation.package = "ethtool"
+        relation.version = "6"
+        relation.release = "1"
+
+        confinfo = pisi.conflict.Conflict(relation)
+        assert not pisi.conflict.installed_package_conflicts(confinfo)
+
+    def testCalculateConflicts(self):
+        packagedb = pisi.db.packagedb.PackageDB()
+        packages = ["ethtool", "zlib", "ctorrent"]
+        assert pisi.conflict.calculate_conflicts(packages, packagedb)
+
+    def testPackageConflicts(self):
+        pkg = ["ethtool","zlib"]
+
+        assert pisi.conflict.package_conflicts(pkg, confs)
+
+
+
