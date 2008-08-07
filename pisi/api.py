eef8d0419b18674569d5ff18faf03c826d45047b
Faik Uygur <faik@pardus.org.tr>
2008-08-07 08:01:01 +0000

    Check validity of repo names
    BUG:FIXED:5906

--- pisi/api.py
+++ pisi/api.py
@@ -10,6 +10,7 @@
 # Please read the COPYING file.
 
 import os
+import re
 import logging
 import logging.handlers
 
@@ -580,6 +581,8 @@ def index(dirs=None, output='pisi-index.xml', skip_sources=False, skip_signing=F
     ctx.ui.info(_('* Index file written'))
 
 def add_repo(name, indexuri, at = None):
+    if not re.match("^[a-zA-Z0-9\\-\\_]*$", name):
+        raise pisi.Error(_('Not a valid repo name.'))
     repodb = pisi.db.repodb.RepoDB()
     if repodb.has_repo(name):
         raise pisi.Error(_('Repo %s already present.') % name)
