1831c400e656d330827cc4e26c8f9f44b4c8c87c
Ozan Çağlayan <ozan@pardus.org.tr>
2009-08-17 10:48:16 +0000

    Replace if by elif, fix indentation problem

--- pisi/util.py
+++ pisi/util.py
@@ -513,7 +513,7 @@ def strip_file(filepath, fileinfo, outpath):
         run_strip(filepath, "--strip-debug")
         return True
 
-    """
+        """
     if "SB relocatable" in fileinfo and filepath.endswith(".ko"):
         # kernel module
         if ctx.config.values.build.generatedebug:
@@ -521,9 +521,9 @@ def strip_file(filepath, fileinfo, outpath):
             save_elf_debug(filepath, outpath)
         run_strip(filepath, "--strip-debug")
         return True
-    """
+        """
 
-    if "SB executable" in fileinfo:
+    elif "SB executable" in fileinfo:
         if ctx.config.values.build.generatedebug:
             check_dir(os.path.dirname(outpath))
             save_elf_debug(filepath, outpath)
