49b49aa0a5da2e9c0a8995f04c1103f5f9b878a7
Faik Uygur <faik@pardus.org.tr>
2009-09-03 12:26:35 +0000

    remove for now

--- pisi/util.py
+++ pisi/util.py
@@ -534,16 +534,6 @@ def strip_file(filepath, fileinfo, outpath):
         run_strip(filepath, "--strip-debug")
         return True
 
-        """
-    if "SB relocatable" in fileinfo and filepath.endswith(".ko"):
-        # kernel module
-        if ctx.config.values.build.generatedebug:
-            check_dir(os.path.dirname(outpath))
-            save_elf_debug(filepath, outpath)
-        run_strip(filepath, "--strip-debug")
-        return True
-        """
-
     elif "SB executable" in fileinfo:
         if ctx.config.values.build.generatedebug:
             check_dir(os.path.dirname(outpath))
