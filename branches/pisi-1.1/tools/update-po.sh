bc37591587793094959e65fc7986ad7549ba4db8
Fatih Aşıcı <fatih@pardus.org.tr>
2009-10-27 10:06:44 +0000

    Move pisi-1.1 branch

--- branches/pisi-1.1/tools/update-po.sh
+++
@@ -1,8 +0,0 @@
-
-find pisi -iname '*.py' | grep -v pisi/cli/commands.py >exclude
-python tools/pygettext.py -D -X exclude -o po/pisi.pot pisi pisi-cli scripts /usr/lib/python2.4/optparse.py
-for lang in po/*.po
-do
-    msgmerge -U $lang po/pisi.pot
-done
-rm exclude
