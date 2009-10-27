57eaa331cb016bb1271801e1d2c5e9edc6720739
Fatih Aşıcı <fatih@pardus.org.tr>
2009-10-27 10:12:57 +0000

    Move pisi-1.0_alpha9 tag

--- tags/pisi-1.0_alpha9/tools/installAllAvailable.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-argNUM=${#}
-if [ ${argNUM} -lt 1 ] ; then
-    echo "At least one repo name needed"
-    exit 1
-fi
-./pisi-cli --ignore-comar install `./pisi-cli list-available $@ | tail -n+3 | xargs`
