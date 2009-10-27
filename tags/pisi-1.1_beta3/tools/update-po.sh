07ea07711314915f1b34a9dceadcad063d43f790
Fatih Aşıcı <fatih@pardus.org.tr>
2009-10-27 10:13:29 +0000

    Move pisi-1.1_beta3 tag

--- tags/pisi-1.1_beta3/tools/update-po.sh
+++
@@ -1,4 +0,0 @@
-find pisi -iname '*.py' | grep -v pisi/cli/commands.py >exclude
-python tools/pygettext.py -D -X exclude -o po/pisi.pot pisi pisi-cli tools
-msgmerge -U po/tr.po po/pisi.pot
-rm exclude
