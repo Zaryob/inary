ddd1b714b58f3a85b4c76d5a6e1771c705a26df5
Faik Uygur <faik@pardus.org.tr>
2009-12-04 13:06:53 +0000

    It works!
    BUG:FIXED:11406

--- scenarios/bug11406scen.py
+++ scenarios/bug11406scen.py
@@ -1,25 +1,42 @@
 # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 #
-# Scenario : bug11406scen.py
+# Scenario : bug11405scen.py
 #
 # Source   : http://bugs.pardus.org.tr/show_bug.cgi?id=11406
 #
+# Problem  : reverse strict deps upgrading not working
+#
+# Problem Description: 
+#
+# pisi updates strict reverse deps if the reverse deps dependencies are not satisfied. For example if kernel is tried
+# to be upgraded all the reverse deps are forced to be upgraded automatically. When only one rev-dep module is
+# selected for an upgrade, kernel also comes as a strict dep for this module. But when kernel comes, all the rev-deps
+# should come with kernel, too. It is not coming.
+#
+# This creates a problem for a user when he/she tries to update only her graphics card driver: kernel comes but none
+# of the other drivers are updated. Sound becomes non-working as well as the other drivers.
+#
+# Expected:
+#
+# rev-deps of all the calculated to be upgraded packages should come
+
 
 from pisi.scenarioapi.scenario import *
 
+MODULE_ALSA_DRIVER = "module-alsa-driver"
+MODULE_FGLRX = "module-fglrx"
 KERNEL = "kernel"
-MODULE_A = "module-abc"
-MODULE_B = "module-def"
 
-let_repo_had(KERNEL, with_version("1.0"), with_requiring_actions("reverseDependencyUpdate"))
-let_repo_had(MODULE_A, with_version("1.0"), with_added_dependency(KERNEL, version="1.0"))
-let_repo_had(MODULE_B, with_version("1.0"), with_added_dependency(KERNEL, version="1.0"))
+let_repo_had(KERNEL, with_version("2.6.30"))
+let_repo_had(MODULE_ALSA_DRIVER, with_added_dependency(KERNEL, version="2.6.30"))
+let_repo_had(MODULE_FGLRX, with_added_dependency(KERNEL, version="2.6.30"))
 
-let_pisi_had(KERNEL, MODULE_A, MODULE_B)
+let_pisi_had(KERNEL, MODULE_ALSA_DRIVER, MODULE_FGLRX)
 
 def run():
-    repo_version_bumped(KERNEL, with_version("2.0"))
-    repo_version_bumped(MODULE_A, with_version("2.0"), with_added_dependency(KERNEL, version="2.0"))
-    repo_version_bumped(MODULE_B,  with_version("2.0"), with_added_dependency(KERNEL, version="2.0"))
+    repo_version_bumped(KERNEL, with_version("2.6.31"))
+    repo_version_bumped(MODULE_ALSA_DRIVER, with_added_dependency(KERNEL, version="2.6.31"))
+    repo_version_bumped(MODULE_FGLRX, with_added_dependency(KERNEL, version="2.6.31"))
     repo_updated_index()
-    pisi_upgraded(MODULE_A)
+    pisi_upgraded(MODULE_FGLRX)
