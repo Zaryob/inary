25eb90f3dd57c1c9f1b35b0dd27edaee7a1f7880
Fatih Aşıcı <fatih@pardus.org.tr>
2010-03-02 07:33:50 +0000

    Take -D parameter into account when looking for file permission changes

--- pisi/operations/delta.py
+++ pisi/operations/delta.py
@@ -157,7 +157,7 @@ def find_permission_changes(oldfiles, newfiles):
     permissions = []
     for h in unchanged:
         for _file in files_new[h]:
-            path = "/%s" % _file.path
+            path = os.path.join(ctx.config.dest_dir(), _file.path)
             if os.path.exists(path) and oct(stat.S_IMODE(os.stat(path)[stat.ST_MODE])) != _file.mode:
                 permissions.append((path, int(_file.mode, 8)))
 
