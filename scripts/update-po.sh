c25e0d703dbda8c0f5b8127256acd78f4e32fbc9
Fatih Aşıcı <fatih@pardus.org.tr>
2010-04-11 11:07:38 +0000

    scripts: Fix update-po.sh script
    This fixes the untranslated strings in --help output.

--- scripts/update-po.sh
+++ scripts/update-po.sh
@@ -1,6 +1,9 @@
+#!/bin/sh
 
-find pisi -iname '*.py' | grep -v pisi/cli/commands.py >exclude
-python tools/pygettext.py -D -X exclude -o po/pisi.pot pisi pisi-cli scripts /usr/lib/python2.4/optparse.py
+PYTHON_VER=`python -c "import sys; print '%d.%d' % sys.version_info[:2]"`
+
+find pisi -iname '*.py' | grep -v pisi/cli/commands.py > exclude
+python scripts/pygettext.py -D -X exclude -o po/pisi.pot pisi pisi-cli /usr/lib/python$PYTHON_VER/optparse.py
 for lang in po/*.po
 do
     msgmerge -U $lang po/pisi.pot
