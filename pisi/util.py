ffe4620ded407c121be18ea6fa418d2f81cdbc3b
Fatih Aşıcı <fatih@pardus.org.tr>
2010-05-18 12:15:51 +0000

    util: Add a singleton metaclass

--- pisi/util.py
+++ pisi/util.py
@@ -32,6 +32,17 @@ import gettext
 __trans = gettext.translation('pisi', fallback=True)
 _ = __trans.ugettext
 
+class Singleton(type):
+    def __init__(cls, name, bases, dict):
+        super(Singleton, cls).__init__(name, bases, dict)
+        cls.instance = None
+
+    def __call__(cls, *args, **kw):
+        if cls.instance is None:
+            cls.instance = super(Singleton, cls).__call__(*args, **kw)
+
+        return cls.instance
+
 # pisi modules
 import pisi
 import pisi.context as ctx
