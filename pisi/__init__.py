0fe53eb3dd64e7dc04a0142c46e2bc5de15976b2
Fatih Aşıcı <fatih@pardus.org.tr>
2010-05-27 08:49:07 +0000

    pisi 2.3
    This is the first stable release of 2.3.x series. This release
    includes assorted bug fixes that might be considered critical.
    
    Full changelog since version 2.2.19 is included in ChangeLog file.
    
    Changes since 2.3_rc1
    ---------------------
    fatih (7):
          install: Call flush/fsync for critical pisi files
          repodb: Do not fail when Distribution tag is missing
          repodb: Do not search index files of local repos in /var/lib/pisi
          scripts: Add a script to generate ChangeLog files from svn log
          scripts/make-changelog: Write bug report links
          changelog: Rename old ChangeLog file
          Remove NEWS file and update MANIFEST.in
    
    ozan.caglayan (1):
          Always use /root/.ccache for CCACHE_DIR for not bypassing ccache when we modify /root in actions.py. A more elegant way is to read CCACHE_DIR from pisi.conf

--- pisi/__init__.py
+++ pisi/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2005 - 2009, TUBITAK/UEKAE
+# Copyright (C) 2005 - 2010, TUBITAK/UEKAE
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free
@@ -18,7 +18,7 @@ import atexit
 import logging
 import logging.handlers
 
-__version__ = "2.3_rc1"
+__version__ = "2.3"
 
 __all__ = [ 'api', 'configfile', 'db']
 
