75b4291d8b73dcda372a811c3ac483ff4322b7ce
Suleyman Poyraz <zaryob.dev@gmail.com>
2018-01-16 17:02:50 +0300

    Bugs are fixed

--- inary/fetcher.py
+++ inary/fetcher.py
@@ -15,8 +15,16 @@ import os
 import time
 import base64
 import shutil
-import urllib.request as urlrequest
-import urllib.error as urlerror
+import socket
+import sys
+
+from base64 import encodestring
+from shutil import move
+
+# Network libraries
+from http.client import HTTPException
+import ftplib
+import urllib.request, urllib.parse, urllib.error 
 
 #Gettext translation library
 import gettext
@@ -29,7 +37,20 @@ import inary.util as util
 import inary.context as ctx
 import inary.uri
 
+# For raising errors when fetching
 class FetchError(inary.Error):
+    pass
+
+# For raising errors when connecting to server
+class RangeError(inary.Error):
+    pass
+
+# For raising errors when opening files
+class FileError(inary.Error):
+    pass
+
+# For raising errors when connecting to server
+class RemoteError(inary.Error):
     pass
 
 class UIHandler:
@@ -46,14 +67,13 @@ class UIHandler:
         self.last_updated    = 0
         self.exist_size      = 0
 
-    def start(self, archive, url, size):
+    def start(self, archive, url, basename, size):
         if os.path.exists(archive):
             self.exist_size = os.path.getsize(archive)
         #self.filename   = util.remove_suffix(ctx.const.partial_suffix, basename)
         self.url        = url
         self.basename   = basename
         self.total_size = size or 0
-        self.text       = text
 
         self.now    = lambda: time.time()
         self.t_diff = lambda: self.now() - self.s_time
@@ -61,7 +81,6 @@ class UIHandler:
         self.s_time = self.now()
 
     def update(self, size):
-
         if self.size == size:
             return
 
@@ -88,7 +107,7 @@ class UIHandler:
     def _update_ui(self):
         ctx.ui.display_progress(operation       = "fetching",
                                 percent         = self.percent,
-                                #filename        = self.filename,
+                                filename        = self.basename,
                                 total_size      = self.total_size or self.size,
                                 downloaded_size = self.size,
                                 rate            = self.rate,
@@ -116,19 +135,19 @@ class Fetcher:
         self.partial_file = os.path.join(self.destdir, self.url.filename()) + ctx.const.partial_suffix
 
         util.ensure_dirs(self.destdir)
-        self.headers_dict = {'user-agent' : 'Inary Fetcher/' + inary.__version__,
-                             'http-headers' : self._get_http_headers()
+        self.headers_dict = {'user-agent' : 'Inary Fetcher/' + inary.__version__
+                             #'http-headers' : self._get_http_headers()
                              #'ftp-headers' : self._get_ftp_headers()
                              }
 
 
     def test(self, timeout=3):
         try:
-            request.urlopen(self.url.get_uri(),
-                         proxies=self._get_proxies(),
-                         timeout=timeout,
-                         headers=headers_dict
-                        )
+            urllib.urlopen(self.url.get_uri(),
+                           proxies=self._get_proxies(),
+                           timeout=timeout,
+                           headers=self.headers_dict
+                           )
 
         except ValueError as e:
             msg = _("Url Problem: \n %s") % e
@@ -147,76 +166,127 @@ class Fetcher:
 
         return True
 
-    def fetch (self):
+    def fetch(self):
         """Return value: Fetched file's full path.."""
-
         if not self.url.filename():
-            raise FetchError(_('Filename error'))
+            raise FileError(_('Filename error'))
 
         if not os.access(self.destdir, os.W_OK):
-            raise FetchError(_('Access denied to write to destination directory: "%s"') % (self.destdir))
+            raise FileError(_('Access denied to write to destination directory: "%s"') % (self.destdir))
 
         if os.path.exists(self.archive_file) and not os.access(self.archive_file, os.W_OK):
-            raise FetchError(_('Access denied to destination file: "%s"') % (self.archive_file))
+            raise FileError(_('Access denied to destination file: "%s"') % (self.archive_file))
 
         else:
+            self.exist_size = 0
+
+        uri = self.url.get_uri()
+        try:
             try:
-                with open(self.partial_file, "wb") as f:
-                    response = request.urlopen(self.url.get_uri(),
-                                        #proxies = self._get_proxies,
-                                        headers = self.headers_dict,
-                                        verify=None,
-                                        stream=True)
+                try:
+                    responseObj = urllib.request.urlopen(self.formatRequest(urllib.request.Request(uri)))
 
-                    handler= UIHandler()
-                    total_length = int(response.headers.get('content-length'))
+                except RangeError:
+                    ctx.ui.info(_('Requested range not satisfiable, starting again.'))
+                    self.exist_size = 0
+                    responseObj = urllib.request.urlopen(self.formatRequest(urllib.request.Request(uri)))
 
-                    if total_length is None:  # no content length header
-                    # just download the file in one go and fake the progress reporting once done
-                        ctx.ui.warning("content-length header is missing for the fetch file, "
-                                    "download progress reporting will not be available")
-                        f.write(response.content)
-                        size = f.tell()
-                        handler.start(self.archive_file, self.url.get_uri(), size)
-                        handler.end(size)
-                    else:
-                        handler.start(self.url.get_uri(), total_length)
-                        bytes_read = 0
-                        for buf in response.iter_content(1024 * 1024):  # 1 MB chunks
-                            if buf:
-                                f.write(buf)
-                                bytes_read += len(buf)
-                                handler.update(bytes_read)
-                        handler.end(bytes_read)
+                headers = responseObj.info()
+                handler= UIHandler()
 
+            except ValueError as e:
+                raise FetchError(_('Could not fetch destination file: "%s" \nRaised Value error: "%s"') % (uri, e))
             except OSError as e:
-                raise FetchError(_('Could not fetch destination file "%s":%s') % (self.url.get_uri(), e))
+                raise FetchError(_('Could not fetch destination file: "%s"; \n"%s"') % (uri, e))
+            except urllib.error.HTTPError as e:
+                raise FetchError(_('Could not fetch destination file: "%s"; \n"%s"') % (uri, e))
+            except urllib.error.URLError as e:
+                raise FetchError(_('Could not fetch destination file: "%s"; \n"%s"') % (uri, e[-1][-1]))
+            except HTTPException as e:
+                raise FetchError(_('Could not fetch destination file: "%s"; ("%s"): "%s"') % (uri, e.__class__.__name__, e))
 
-            except request.exceptions.InvalidSchema:
-                raise FetchError(_('Package manager not support downloding from ftp mirror'))
+        except FetchError as e:
+            raise FetchError(_('A problem occurred. Please check the archive address and/or permissions again. %s') %e)
 
+        total_length = int(headers['Content-Length'])
+
+        bs=1024 #for 1MB
+        chunk = responseObj.read(bs)
+        downloaded_size = 0
+
+        with open(self.partial_file, 'wb') as dest:
+            if total_length:
+                handler.start(self.archive_file, self.url.get_uri(), self.url.filename(), total_length)
+                while chunk:
+                    chunk = responseObj.read(bs)
+                    dest.write(chunk)
+                    downloaded_size += len(chunk)
+                    handler.update(downloaded_size)
+                handler.end(downloaded_size)
+            else:
+                ctx.ui.warning("Content-length header is missing for the fetch file, Download progress reporting will not be available")
+                size=dest.tell()
+                handler.start(self.archive_file, self.url.get_uri(), self.url.filename(), size)
+                while chunk:
+                    chunk = responseObj.read(bs)
+                    dest.write(chunk)
+                    size=dest.tell()
+                    handler.update(size)
+                handler.end(size)
 
         if os.stat(self.partial_file).st_size == 0:
             os.remove(self.partial_file)
-            raise FetchError(_('A problem occurred. Please check the archive address and/or permissions again.'))
 
         shutil.move(self.partial_file, self.archive_file)
 
         return self.archive_file
+
+    def formatRequest(self, request):
+        if self.url.auth_info():
+            enc = encodestring('%s:%s' % self.url.auth_info())
+            request.add_header('Authorization', 'Basic %s' % enc)
+
+        range_handlers = {
+            'http' : HTTPRangeHandler,
+            'https': HTTPRangeHandler,
+            'ftp' : FTPRangeHandler
+        }
+
+        if self.exist_size and self.scheme in range_handlers:
+            opener = urllib.request.build_opener(range_handlers.get(self.scheme)())
+            urllib.request.install_opener(opener)
+            request.add_header('Range', 'bytes=%d-' % self.exist_size)
+
+        proxy_handler = None
+        # FIXME: Should use __get_proxies function in here
+        if ctx.config.values.general.http_proxy and self.url.scheme() == "http":
+            http_proxy = ctx.config.values.general.http_proxy
+            proxy_handler = urllib.request.ProxyHandler({URI(http_proxy).scheme(): http_proxy})
+        elif ctx.config.values.general.https_proxy and self.url.scheme() == "https":
+            https_proxy = ctx.config.values.general.https_proxy
+            proxy_handler = urllib.request.ProxyHandler({URI(https_proxy): https_proxy})
+        elif ctx.config.values.general.ftp_proxy and self.url.scheme() == "ftp":
+            ftp_proxy = ctx.config.values.general.ftp_proxy
+            proxy_handler = urllib.request.ProxyHandler({URI(http_proxy): ftp_proxy})
+        if proxy_handler:
+            ctx.ui.info(_("Proxy configuration has been found for '%s' protocol") % self.url.scheme())
+            opener = urllib.request.build_opener(proxy_handler)
+            urllib.request.install_opener(opener)
+        return request
 
     def _get_http_headers(self):
         headers = []
         if self.url.auth_info() and (self.url.scheme() == "http" or self.url.scheme() == "https"):
             enc = base64.encodestring('%s:%s' % self.url.auth_info())
             headers.append(('Authorization', 'Basic %s' % enc),)
-        return bytearray(headers)
-#
-#    def _get_ftp_headers(self):
-#        headers = []
-#        if self.url.auth_info() and self.url.scheme() == "ftp":
-#            enc = base64.encodestring('%s:%s' % self.url.auth_info())
-#            headers.append(('Authorization', 'Basic %s' % enc),)
-#        return bytearray(headers)
+        return tuple(headers)
+
+    def _get_ftp_headers(self):
+        headers = []
+        if self.url.auth_info() and self.url.scheme() == "ftp":
+            enc = base64.encodestring('%s:%s' % self.url.auth_info())
+            headers.append(('Authorization', 'Basic %s' % enc),)
+        return tuple(headers)
 
     def _get_proxies(self):
         proxies = {}
@@ -227,8 +297,8 @@ class Fetcher:
         if ctx.config.values.general.https_proxy and self.url.scheme() == "https":
             proxies[inary.uri.URI(ctx.config.values.general.https_proxy).scheme()] = ctx.config.values.general.https_proxy
 
-        #if ctx.config.values.general.ftp_proxy and self.url.scheme() == "ftp":
-        #    proxies[inary.uri.URI(ctx.config.values.general.ftp_proxy).scheme()] = ctx.config.values.general.ftp_proxy
+        if ctx.config.values.general.ftp_proxy and self.url.scheme() == "ftp":
+            proxies[inary.uri.URI(ctx.config.values.general.ftp_proxy).scheme()] = ctx.config.values.general.ftp_proxy
 
         if self.url.scheme() in proxies:
             ctx.ui.info(_("Proxy configuration has been found for '%s' protocol") % self.url.scheme())
@@ -247,7 +317,6 @@ class Fetcher:
         if not os.path.exists(self.partial_file):
             return None
 
-        import urllib.request, urllib.error, urllib.parse
         try:
             file_obj = urllib.request.urlopen(urllib.request.Request(self.url.get_uri()))
         except urllib.error.URLError:
@@ -264,7 +333,115 @@ class Fetcher:
             os.remove(self.partial_file)
             return None
 
+class HTTPRangeHandler(urllib.request.BaseHandler):
+    """
+    to override the urllib2 error: 'Error 206: Partial Content'
+    this reponse from the HTTP server is already what we expected to get.
+    Don't give up, resume downloading..
+    """
+    def http_error_206(self, request, fp, errcode, msg, headers):
+        return urllib.addinfourl(fp, headers, request.get_full_url())
+    def http_error_416(self, request, fp, errcode, msg, headers):
+        # HTTP 1.1's 'Range Not Satisfiable' error..
+        raise RangeError 
 
+class FTPRangeHandler(urllib.request.FTPHandler):
+    """
+    FTP Range support..
+    """
+    def ftp_open(self, req):
+        host = req.get_host()
+        host, port = urllib.parse.splitport(host)
+        if port is None:
+            port = ftplib.FTP_PORT
+        try:
+            host = socket.gethostbyname(host)
+        except socket.error as msg:
+            raise FetchError(msg)
+        path, attrs = urllib.parse.splitattr(req.get_selector())
+        dirs = path.split('/')
+        dirs = list(map(urllib.parse.unquote, dirs))
+        dirs, file = dirs[:-1], dirs[-1]
+        if dirs and not dirs[0]:
+            dirs = dirs[1:]
+        try:
+            fw = self.connect_ftp('', '', host, port, dirs)
+            type = file and 'I' or 'D'
+            for attr in attrs:
+                attr, value = urllib.parse.splitattr(attr)
+                if attr.lower() == 'type' and \
+                   value in ('a', 'A', 'i', 'I', 'd', 'D'):
+                    type = value.upper()
+
+            rawr = req.headers.get('Range', None)
+            if rawr:
+                rest = int(rawr.split("=")[1].rstrip("-"))
+            else:
+                rest = 0
+            fp, retrlen = fw.retrfile(file, type, rest)
+
+            fb, lb = rest, retrlen
+            if retrlen is None or retrlen == 0:
+                raise RangeError
+            retrlen = lb - fb
+            if retrlen < 0:
+                # beginning of range is larger than file
+                raise RangeError
+
+            headers = ''
+            mtype = guess_type(req.get_full_url())[0]
+            if mtype:
+                headers += 'Content-Type: %s\n' % mtype
+            if retrlen is not None and retrlen >= 0:
+                headers += 'Content-Length: %d\n' % retrlen
+
+            from io import StringIO
+
+            return urllib.addinfourl(fp, Message(StringIO(headers)), req.get_full_url())
+        except ftplib.all_errors as msg:
+            raise IOError(_('ftp error'), msg).with_traceback(sys.exc_info()[2])
+
+    def connect_ftp(self, user, passwd, host, port, dirs):
+        fw = ftpwrapper('', '', host, port, dirs)
+        return fw
+
+class ftpwrapper(urllib.request.ftpwrapper):
+    def retrfile(self, file, type, rest=None):
+        self.endtransfer()
+        if type in ('d', 'D'): cmd = 'TYPE A'; isdir = 1
+        else: cmd = 'TYPE ' + type; isdir = 0
+        try:
+            self.ftp.voidcmd(cmd)
+        except ftplib.all_errors:
+            self.init()
+            self.ftp.voidcmd(cmd)
+        conn = None
+        if file and not isdir:
+            try:
+                self.ftp.nlst(file)
+            except ftplib.error_perm as reason:
+                raise IOError(_('ftp error'), reason).with_traceback(sys.exc_info()[2])
+            # Restore the transfer mode!
+            self.ftp.voidcmd(cmd)
+            try:
+                cmd = 'RETR ' + file
+                conn = self.ftp.ntransfercmd(cmd, rest)
+            except ftplib.error_perm as reason:
+                if str(reason)[:3] == '501':
+                    # workaround for REST not suported error
+                    fp, retrlen = self.retrfile(file, type)
+                    fp = RangeableFileObject(fp, (rest,''))
+                    return (fp, retrlen)
+                elif str(reason)[:3] != '550':
+                    raise IOError(_('ftp error'), reason).with_traceback(sys.exc_info()[2])
+        if not conn:
+            self.ftp.voidcmd('TYPE A')
+            if file: cmd = 'LIST ' + file
+            else: cmd = 'LIST'
+            conn = self.ftp.ntransfercmd(cmd)
+        self.busy = 1
+        return (urllib.addclosehook(conn[0].makefile('rb'),
+                            self.endtransfer), conn[1])
 # helper function
 def fetch_url(url, destdir, progress=None, destfile=None):
     fetch = Fetcher(url, destdir, destfile)
