e3f6e3518e1eed96e81be0edd636349bf5bf70eb
Your Name <you@example.com>
2019-07-03 16:23:09 +0300

    Bağımlılık çözme algoritması 100 kat hızlandırıldı.

---
+++ inary/misc/mergesort.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+#Merge sort by sulincix
+def msort(x):
+    result = []
+    if len(x) < 2:
+        return x
+    mid = int(len(x)/2)
+    y = msort(x[:mid])
+    z = msort(x[mid:])
+    while (len(y) > 0) or (len(z) > 0):
+        if len(y) > 0 and len(z) > 0:
+            if y[0] > z[0]:
+                result.append(z[0])
+                z.pop(0)
+            else:
+                result.append(y[0])
+                y.pop(0)
+        elif len(z) > 0:
+            for i in z:
+                result.append(i)
+                z.pop(0)
+        else:
+            for i in y:
+                result.append(i)
+                y.pop(0)
+    return result
