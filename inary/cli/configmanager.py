5600d4a9eeeb72c75f8be41cfd97f5283e58755e
Suleyman Poyraz <zaryob.dev@gmail.com>
2019-08-26 14:41:36 +0300

    Auto stash before revert of "Lisans Güncellendi:"

--- inary/cli/configmanager.py
+++ inary/cli/configmanager.py
@@ -3,14 +3,14 @@
 # Copyright (C) 2019, Suleyman POYRAZ (Zaryob)
 #
 # This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU Affero General Public License as published by the Free
+# the terms of the GNU General General Public License as published by the Free
 # Software Foundation; either version 3 of the License, or (at your option)
 # any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+# GNU General General Public License for more details.
 #
 # Please read the COPYING file.
 #
