9d99f756798976e2974f0c73414a470d7dcd0d9f
Suleyman Poyraz <zaryob.dev@gmail.com>
2019-09-07 14:48:50 +0300

    Fixed error when installing inary from file
    If an inary  have being installed form file, the directory names should be cleaned to get "just" name of package.

--- inary/cli/install.py
+++ inary/cli/install.py
@@ -111,7 +111,7 @@ expanded to package names.
         install.install(packages, ctx.get_option('reinstall') or reinstall)
 
 
-        config_changes = helper.check_config_changes([util.parse_package_name_legacy(i)[0] for i in packages])
+        config_changes = helper.check_config_changes([util.parse_package_name_legacy(i.split("/")[-1])[0] for i in packages])
 
         if config_changes:
             if ctx.ui.confirm(_("[!] Some config files have been changed. Would you like to see and apply them?")):
