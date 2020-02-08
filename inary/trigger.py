c5ca775209e5a69d7f8a8358940d703327eb16cc
Suleyman Poyraz <zaryob.dev@gmail.com>
2020-02-08 18:45:46 +0300

    Şimdilik bir şunu deneyelim.

--- inary/trigger.py
+++ inary/trigger.py
@@ -41,15 +41,14 @@ class Trigger:
         """"""
         curDir = os.getcwd()
         os.chdir(ctx.config.dest_dir())
-
-        ret_val=os.system('python3 -c \'import postoperations\nif(hasattr(postoperations,"{0}")):\n postoperations.{0}()\''.format(func))
-        if ret_val != 0:
-            os.chdir(curDir)
-            return False
+        if func in self.Locals:
+            self.Locals[func]()
 
         else:
-            os.chdir(curDir)
-            return True
+            pass
+
+        os.chdir(curDir)
+        return True
 
     def preinstall(self, specdir):
         self.specdir=specdir
