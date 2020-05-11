8da6afb10dbdcd12957101e18be7da6a7045cc71
Suleyman Poyraz <zaryob.dev@gmail.com>
2020-05-11 13:59:58 +0000

    add-repository komut hatası giderildi.

--- inary/cli/addrepo.py
+++ inary/cli/addrepo.py
@@ -25,6 +25,7 @@ import inary.context as ctx
 import inary.db
 import inary.errors
 
+import inary.operations.repository as repository
 
 class AddRepo(command.Command, metaclass=command.autocommand):
     __doc__ = _("""Add a repository
@@ -61,7 +62,6 @@ NB: We support only local files (e.g., /a/b/c) and http:// URIs at the moment
         repository.remove_repo(repo)
 
     def run(self):
-        import inary.operations.repository as repository
         if len(self.args) == 2:
             self.init()
             name, indexuri = self.args
