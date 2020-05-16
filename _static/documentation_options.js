5f3df83ddd7ee88ac90fb6e1efbe3e47201201da
Suleyman Poyraz <zaryob.dev@gmail.com>
2020-05-17 00:30:01 +0300

    Bazı düzenlemeler.

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -3,7 +3,9 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '1.2_alpha',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: false,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
