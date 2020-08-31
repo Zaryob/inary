57b1322ea13a4691a178e54a5b82fc49e5faccbc
frknkrc44 <krc440002@gmail.com>
2020-08-31 03:34:07 +0300

    Bug fixes 40

--- inary/util/process.py
+++ inary/util/process.py
@@ -15,8 +15,6 @@
 """misc. utility functions, including process and file utils"""
 
 # Inary Modules
-import inary
-import inary.errors
 import inary.context as ctx
 from os import path, makedirs as mkdirs, environ, access, X_OK
 
